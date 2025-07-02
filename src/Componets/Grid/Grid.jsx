cc9eca8a53e96ea21599d9c19c5b86809bc302d5
Hansraj Saini <hanssaini2005@gmail.com>
2025-07-03 04:19:01 +0530

    Initial commit

--- src/Componets/Grid/Grid.jsx
+++ src/Componets/Grid/Grid.jsx
@@ -1,47 +1,59 @@
-import{ useState } from 'react';
+
+import { useState } from 'react';
 import Card from '../Card/Card';
 import './Grid.css';
 import isWinner from '../../helpers/ckeckWinner';
-function Grid({numberOfCards}){
- const [board, setBoard] = useState(Array(numberOfCards).fill(""));
- const [turn, setTurn] = useState(true);
- const [winner, setWinner] = useState(null);
- function play(index){
-  if (turn==true){
-    board[index] = "o"; 
-  }else{
-    board[index] = 'x';
-  }
-  const win = isWinner(board, board[index]);
-  if (win) {
-    setWinner(win);
-  }
-  setBoard([...board]);
-  setTurn(!turn);
- }
- function reset(){
-  setBoard(Array(numberOfCards).fill(""));
-  setTurn(true);
-  setWinner(null);
- }
 
- return (
-  <div className='grid-wrapper'>
-    <h3 className='turn-highlight'>Current Turn: {(turn)?'o':'x'}</h3>
-    <div className='grid'>
-      {board.map((el, index) => <Card gameEnd={winner? true : false} key={index} onPlay={play} player={el} index={index} />)}
-     
-    </div>
-    {
-    winner &&(
-        <div className='winner-wrapper'>
-        <h3 className='winner-highlight'>Winner is: {winner}</h3>
-        <button className='reset' onClick={reset}>Reset</button>
-        </div>
-      )
+function Grid({ numberOfCards }) {
+  const [board, setBoard] = useState(Array(numberOfCards).fill(""));
+  const [turn, setTurn] = useState(true); // true for 'o', false for 'x'
+  const [winner, setWinner] = useState(null);
+
+  function play(index) {
+    if (board[index] !== "" || winner) return; // Don't allow moves after game ends
+
+    const newBoard = [...board];
+    newBoard[index] = turn ? "o" : "x"; // 'o' for true, 'x' for false
+    setBoard(newBoard);
+
+    const win = isWinner(newBoard, newBoard[index]);
+    if (win) {
+      setWinner(win);
     }
-  </div>
- )
 
+    setTurn(!turn);
+  }
+
+  function reset() {
+    setBoard(Array(numberOfCards).fill(""));
+    setTurn(true);
+    setWinner(null);
+  }
+
+  // Draw condition: if the board is full and there's no winner
+  const isDraw = board.every(cell => cell !== "") && !winner;
+
+  return (
+    <div className='grid-wrapper'>
+      <h3 className='turn-highlight'>Current Turn: {turn ? 'o' : 'x'}</h3>
+      <div className='grid'>
+        {board.map((el, index) => (
+          <Card gameEnd={!!winner || isDraw} key={index} onPlay={play} player={el} index={index} />
+        ))}
+      </div>
+
+      {(winner || isDraw) && (
+        <div className='winner-wrapper'>
+          {winner ? (
+            <h3 className='winner-highlight'>Winner is: {winner}</h3>
+          ) : (
+            <h3 className='winner-highlight'>It's a draw!</h3>
+          )}
+          <button className='reset' onClick={reset}>Reset</button>
+        </div>
+      )}
+    </div>
+  );
 }
+
 export default Grid;
